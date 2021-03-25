c05c5daeb50262dec4fff6fb4f1dda65619f2334
Kata Gulyás <kata.gulyas@stud.uni-corvinus.hu>
2021-03-25 12:31:55 +0100

    hetedik heti feladat

---
+++ HajosTeszt/HajosTeszt/wwwroot/hetedik.js
@@ -0,0 +1,26 @@
+﻿window.onload = letöltés()
+var kérdések;
+
+function letöltés() {
+    fetch('questions.json')
+        .then(response => response.json())
+        .then(data => letöltésBefejeződött(data));
+}
+
+function letöltésBefejeződött(d) {
+     console.log("Sikeres letöltés")
+     console.log(d)
+     kérdések = d;
+     kérdésMegjelenítés (0)
+}
+
+function kérdésMegjelenítés(k) {
+    let ide_kérdés = document.getElementById("kérdés_szöveg");
+    ide_kérdés.InnerHTML = kérdések[k].questionText;
+    console.log(`$ {kérdés.lenght} kérdés érkezett}`)
+
+    for (var i = 1; i < 4; i++) {
+        let elem_kérdés = document.createElement("válasz"+i)
+        elem_kérdés.innerHTML = kérdések[k]["answer"+i]
+    }
+}
